f3a5e4c94c18bc8e5d651712a919f7cb591cfafe
SerhiiMolodets <mr.molodets@gmail.com>
2024-11-22 11:25:21 +0200

    add project

---
+++ BreedApp/BreedApp/BreedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreedAppApp.swift
+//  BreedApp
+//
+//  Created by Serhii Molodets on 22.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct BreedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
